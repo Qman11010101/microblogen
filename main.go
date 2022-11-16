3a9c73e0c3c2893423bd5378530f30d7e9e2df49
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2022-11-16 15:57:14 +0900

    wip

--- main.go
+++ main.go
@@ -48,19 +48,21 @@ func fileExists(name string) bool {
 func main() {
 	fmt.Println("microblogen v" + VERSION)
 
-	// Loading config.json
+	// arguments := os.Args[1:]
+
+	// config.json読み込み
 	if !fileExists(configFile) {
 		fmt.Println("Error: Missing " + configFile)
 		os.Exit(1)
 	}
 
-	f, err := os.ReadFile(configFile)
+	configFileBytes, err := os.ReadFile(configFile)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	err = json.Unmarshal([]byte(f), &Config)
+	err = json.Unmarshal([]byte(configFileBytes), &Config)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -69,43 +71,70 @@ func main() {
 	client := microcms.New(Config.Servicedomain, Config.Apikey)
 
 	// 新規記事・更新記事のチェック
-	var minimumContent ContentList
+	var latestContentsInfo ContentList
 
+	// 公開されていない記事は載らない
 	err = client.List(
 		microcms.ListParams{
 			Endpoint: "article",
 			// Fields:   []string{"id", "title", "publishedAt", "updatedAt", "category.id", "category.name"},
 			Fields: []string{"id", "title", "publishedAt", "updatedAt", "category.id"},
 			Limit:  10000, // 無料枠リミット
-		}, &minimumContent)
+		}, &latestContentsInfo)
 
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	fmt.Println(minimumContent)
-
-	for i := 0; i < len(minimumContent.Contents); i++ {
-		fmt.Printf("%+v\n", minimumContent.Contents[i])
+	// TODO: あとで消す
+	fmt.Println("Latest Contents:")
+	for i := 0; i < len(latestContentsInfo.Contents); i++ {
+		fmt.Printf("%+v\n", latestContentsInfo.Contents[i])
 	}
+	// TODO: あとで消す
 
-	// 古いJSONがなければこれのcontentsだけ保存して、あればそれと比較して更新があればレンダリングする
+	articlesJsonPath := Config.Exportpath + "/articles.json"
 
-	// 古いJSONとの比較
-	if !fileExists(Config.Exportpath + "/articles.json") {
-		fp, err := os.Create(Config.Exportpath + "/articles.json")
+	// 記事のJSONがなければ生成して書き込む
+	if !fileExists(articlesJsonPath) {
+		articlesFile, err := os.Create(articlesJsonPath)
 		if err != nil {
 			fmt.Println("Error: ", err)
 			return
 		}
-		defer fp.Close()
+		defer articlesFile.Close()
 
-		s, err := json.Marshal(minimumContent.Contents)
+		s, err := json.Marshal(latestContentsInfo.Contents)
 		if err != nil {
 			fmt.Println("Error: ", err)
 			return
 		}
-		fp.WriteString(string(s))
+		articlesFile.WriteString(string(s))
 	}
+
+	var currentArticles []Content
+
+	articlesJsonBytes, err := os.ReadFile(articlesJsonPath)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	err = json.Unmarshal([]byte(articlesJsonBytes), &currentArticles)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Current Contents:")
+	for i := 0; i < len(currentArticles); i++ {
+		fmt.Printf("%+v\n", currentArticles[i])
+	}
+
+	// 差分レンダリング条件:
+	// ・IDに該当する記事がない
+	// ・UpdatedAtが違う
+
+	//
 }
